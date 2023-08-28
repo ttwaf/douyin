34eb042ef0bac95e1e8eaf416a7388e314e6d7a1
Huan Xue <3274739376@qq.com>
2023-08-28 16:07:03 +0800

    Revert "尝试修改user接口前，做一次备份"
    This reverts commit 6452c7bd0130c9e82a2caf6ac4378989d7a340f5.

--- douyin/routers/routers.go
+++ douyin/routers/routers.go
@@ -6,19 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRouter() *gin.Engine {
+func SetupRouter() *gin.Engine  {
 
-	r := gin.Default() //创建局部路由
+
+	r := gin.Default()//创建局部路由
 	//创建路由组
 	douyin := r.Group("/douyin")
-	douyin.POST("/user/register", controller.SignupHandler) //注册业务的路由
-	douyin.POST("/user/login", controller.LoginHandler)     //登录业务的路由
+	douyin.POST("/user/register",controller.SignupHandler)//注册业务的路由
+	douyin.POST("/user/login",controller.LoginHandler)//登录业务的路由
 	//中间件开发。登录之前可以用下面的功能
 	//登录后会认证。token正确的话就可以访问这些功能，不正确的话就打回去
-	douyin.Use(middlewares.JWTAuthMiddleware()) //应用JWT中间件
+	douyin.Use(middlewares.JWTAuthMiddleware())//应用JWT中间件
 	{
 
 	}
 
 	return r
 }
+
